58cafe5e17ea81d640a666f3812d027775120215
swati.gupta <swati.gupta@BA-000124984.TI.ads>
2021-02-08 21:24:25 +0530

    Reading file from project resources rather from local machine.

--- src/main/java/SampleClient.java
+++ src/main/java/SampleClient.java
@@ -13,15 +13,19 @@ import ca.uhn.fhir.rest.client.api.IClientInterceptor;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import ca.uhn.fhir.rest.client.api.IHttpRequest;
 import ca.uhn.fhir.rest.client.api.IHttpResponse;
+import ca.uhn.fhir.rest.client.interceptor.LoggingInterceptor;
 import ca.uhn.fhir.util.StopWatch;
 
 public class SampleClient implements IClientInterceptor {
-	ArrayList<String> lastNameList = null;
+	ArrayList<String> lastNameList = null;	//stores name read from file to iterate over
 	static Long everyRequestsTime = 0L;
-
+	private static final org.slf4j.Logger myLog = org.slf4j.LoggerFactory.getLogger(SampleClient.class);
+	
 	public static void main(String[] theArgs) {
 		SampleClient cl = new SampleClient();
+		//Reading names from file
 		cl.readLastNameFromFile();
+		//Making requests in loop
 		cl.makeRequest();
 
 	}
@@ -32,33 +36,37 @@ public class SampleClient implements IClientInterceptor {
 			FhirContext fhirContext = FhirContext.forR4();
 			IGenericClient client = fhirContext
 					.newRestfulGenericClient("http://hapi.fhir.org/baseR4");
+			
 			client.registerInterceptor(new SampleClient());
-			// client.registerInterceptor(new LoggingInterceptor(false));
+			client.registerInterceptor(new LoggingInterceptor(false));
 
+			
 			CacheControlDirective c = new CacheControlDirective();
 			c.setNoCache(false);
 			for (int i = 0; i < 3; i++) {
 				for (String name : lastNameList) {
-					System.out.println(i + ", for name :: " + name);
-					if (i == 2) c.setNoCache(true);
+					if (i == 2)
+						c.setNoCache(true);		//Disabling cache
 					client.search().forResource("Patient")
-							.where(Patient.FAMILY.matches().value(name))
+							.where(Patient.FAMILY.matches().value(name))//inside name, sending names from file
 							.returnBundle(Bundle.class).cacheControl(c)
 							.execute();
-				};
-				System.out.println("Average Time for " + i + "::::  "
+				}
+				;
+				myLog.info("Average Time for iteration " + i + "::::  "
 						+ everyRequestsTime / lastNameList.size());
 				everyRequestsTime = 0L;
 			}
-		}else{
-			System.out.println("File is empty. There are no names found in files.");
+		} else {
+			myLog.info("File is empty. There are no names found in files.");
 		}
 	}
 
 	public void readLastNameFromFile() {
 		try {
-			File myObj = new File(
-					"C:\\Users\\swati.gupta.TI\\Desktop\\LastName.txt");
+			String fileName = "lastName.txt";
+			ClassLoader classLoader = getClass().getClassLoader();
+			File myObj = new File(classLoader.getResource(fileName).getFile());
 			Scanner myReader = new Scanner(myObj);
 			lastNameList = new ArrayList<>();
 			while (myReader.hasNextLine()) {
@@ -69,7 +77,7 @@ public class SampleClient implements IClientInterceptor {
 			//lastNameList.forEach(System.out::println);
 			myReader.close();
 		} catch (FileNotFoundException e) {
-			System.out.println("An error occurred.");
+			myLog.error("An error occurred.");
 			e.printStackTrace();
 		}
 	}
@@ -77,14 +85,13 @@ public class SampleClient implements IClientInterceptor {
 	@Override
 	public void interceptRequest(IHttpRequest theRequest) {
 		// Nothing to do before sending request
-		System.out.println("interceptRequest....");
 	}
-	
+
 	@Override
 	public void interceptResponse(IHttpResponse theResponse) throws IOException {
 		StopWatch s = theResponse.getRequestStopWatch();
 		everyRequestsTime = everyRequestsTime + s.getMillis();
-		System.out.println("==========================Time taken: "
+		myLog.info("==========================Time taken: "
 				+ s.getMillis()
 				+ " ms.=========================================");
 	}
